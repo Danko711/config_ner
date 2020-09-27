d2ef2f4db926b42cd636cffcbaa13f50a8ca153e
Daniil <daniil@MacBook-Air-Daniil.local>
2020-09-27 23:10:56 +0300

    .

--- prepare.py
+++ prepare.py
@@ -12,9 +12,9 @@ texts = pd.Series([i[0] for i in data['train']])
 tags = pd.Series([i[1] for i in data['train']])
 
 print('start loading fasttext')
-ft_vectors = gensim.models.fasttext.load_facebook_model('../data/fasttext/fasttext/wiki.simple.bin')
+ft_vectors = gensim.models.fasttext.load_facebook_model('./data/fasttext/fasttext/wiki.simple.bin')
 print('Fasttext loaded')
 vectorizer = Vectorizer(texts=texts, tags=tags, word_embedder=ft_vectors)
 print('vectorizer ready')
-with open('../data/vectorizer/vect.pkl', 'wb') as f:
+with open('./data/vectorizer/vect.pkl', 'wb') as f:
     pickle.dummp(vectorizer, f)
