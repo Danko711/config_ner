f79a47eec6daf046bde70424d159c49f34d95422
Daniil <daniil@MacBook-Air-Daniil.local>
2020-09-28 02:50:29 +0300

    .

--- src/experiment.py
+++ src/experiment.py
@@ -17,16 +17,10 @@ class Experiment(ConfigExperiment):
         reader = Conll2003DatasetReader()
         data = reader.read(dataset_name='conll2003', data_path='./')
 
-        texts = pd.Series([i[0] for i in data['train']])
-        tags = pd.Series([i[1] for i in data['train']])
 
-        print('start loading fasttext')
-        ft_vectors = gensim.models.fasttext.load_facebook_model('../fasttext/wiki.simple.bin')
-        print('Fasttext loaded')
-        vectorizer = Vectorizer(texts=texts, tags=tags, word_embedder=ft_vectors)
-        print('vectorizer ready')
-        with open('../data/vectorizer/vect.pkl', 'wb') as f:
-            pickle.dummp(vectorizer, f)
+        with open(vectorizer_path, 'rb') as f:
+            vectorizer = pickle.load(f)
+
 
         datasets = OrderedDict()
 
