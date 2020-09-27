10618d30c60a0a3fe95d07f3f999fac18850d70d
Daniil <daniil@MacBook-Air-Daniil.local>
2020-09-28 02:57:53 +0300

    .

--- src/dataset.py
+++ src/dataset.py
@@ -1,15 +1,14 @@
 import pandas as pd
 from torch.utils.data import Dataset
 
-from .vectorizer_orig import Vectorizer
 
 
 class ConllDataset(Dataset):
-    def __init__(self, data, stage):
+    def __init__(self, data, stage, vectorizer):
         self.stage = stage
         self.texts = pd.Series([i[0] for i in data[stage]])
         self.tags = pd.Series([i[1] for i in data[stage]])
-        self.vectorizer = Vectorizer()
+        self.vectorizer = vectorizer
 
     def __getitem__(self, idx):
 
