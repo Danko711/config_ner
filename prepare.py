918878549bf674a552e6471abe554457a0a4987e
Daniil <daniil@inca-0120102.sigma.sbrf.ru>
2020-09-28 10:49:13 +0300

    .

--- prepare.py
+++ prepare.py
@@ -5,16 +5,21 @@ import gensim
 from src.data import Conll2003DatasetReader
 from src.vectorizer_orig import Vectorizer
 
-reader = Conll2003DatasetReader()
-data = reader.read(dataset_name='conll2003', data_path='./')
 
-texts = pd.Series([i[0] for i in data['train']])
-tags = pd.Series([i[1] for i in data['train']])
+def main():
+    reader = Conll2003DatasetReader()
+    data = reader.read(dataset_name='conll2003', data_path='./')
 
-print('start loading fasttext')
-ft_vectors = gensim.models.fasttext.load_facebook_model('./fasttext/wiki.simple.bin')
-print('Fasttext loaded')
-vectorizer = Vectorizer(texts=texts, tags=tags, word_embedder=ft_vectors)
-print('vectorizer ready')
-with open('vect.pickle', 'wb') as f:
-    pickle.dump(vectorizer, f)
+    texts = pd.Series([i[0] for i in data['train']])
+    tags = pd.Series([i[1] for i in data['train']])
+
+    print('start loading fasttext')
+    ft_vectors = gensim.models.fasttext.load_facebook_model('./fasttext/wiki.simple.bin')
+    print('Fasttext loaded')
+    vectorizer = Vectorizer(texts=texts, tags=tags, word_embedder=ft_vectors)
+    print('vectorizer ready')
+    with open('vect.pickle', 'wb') as f:
+        pickle.dump(vectorizer, f)
+
+if __name__ == "__main__":
+    main()
