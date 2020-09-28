bbefa79b6661e2b4c78fe09e7b9ba808bbba1916
Daniil <daniil@inca-0120102.sigma.sbrf.ru>
2020-09-28 11:28:19 +0300

    .

--- src/__init__.py
+++ src/__init__.py
@@ -2,13 +2,14 @@ from .experiment import Experiment
 from catalyst.dl import SupervisedRunner as Runner
 from catalyst.dl import registry
 from torch.optim import Adam
+from torch.nn import NLLLoss
 import gensim
 
 from .vectorizer_orig import Vectorizer
 from .model import LstmCrf
-from callbacks.nll_loss import CrfNllCallback
+#from callbacks.nll_loss import CrfNllCallback
 from .runner import CustomRunner as Runner
 
 registry.Model(LstmCrf)
 registry.Optimizer(Adam)
-registry.Criterion(CrfNllCallback)
+registry.Criterion(NLLLoss)
